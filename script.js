fc85063b3a35ffe706eb3fe40c92cc5492ab3092
joshuacdj <joshuacheungdj@gmail.com>
2024-06-06 17:00:36 +0800

    Tweaked the addSquaresToContainer function so that now it should autoresize the grid whilst keeping the dimensions the same for any i by j grid

--- script.js
+++ script.js
@@ -13,12 +13,25 @@ function getSquare() {
 
 function addSquaresToContainer(count) {
 
-    for (let i = 0; i < count; i++) {
 
-        container.append(getSquare());
+    for (let i = 0; i < count; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("class", "row");
+
+        while(row.firstChild){
+            row.removeChild(row.firstChild);
+        }
+
+        for (let j = 0; j < count; j++) {
+
+            row.append(getSquare());
+        }
+
+        container.append(row);
     }
+    
 }
 
 const GRID_SIZE = 16;
 
-addSquaresToContainer(GRID_SIZE * GRID_SIZE);
+addSquaresToContainer(GRID_SIZE);
